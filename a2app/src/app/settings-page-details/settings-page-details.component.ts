8f5c00ae181ec86d2a3f4a70c7840600ab175310
Artem Deikun <artem.deikun@zorallabs.com.ua>
2017-08-18 21:38:38 +0300

    Category list in settings

--- a2app/src/app/settings-page-details/settings-page-details.component.ts
+++ a2app/src/app/settings-page-details/settings-page-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-settings-page-details',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsPageDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  wallet;
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.wallet = params['id'];
+    });
   }
 
 }
--- a2app/src/app/settings/share-wallet/share-wallet.component.ts
+++ a2app/src/app/settings/share-wallet/share-wallet.component.ts
@@ -3,7 +3,6 @@ import { AngularFireDatabase } from 'angularfire2/database';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ShareWalletFormService } from "./share-wallet-form.service";
 import { StatusService } from "../../core/status.service";
-import { ActivatedRoute, Router } from '@angular/router';
 
 declare var $: any;
 
@@ -14,47 +13,37 @@ declare var $: any;
 })
 export class ShareWalletComponent implements OnInit {
 
+  @Input() wallet;
+
   private shareWalletForm;
-
   private walletList;
-
   private shareWallet = function ({ value, valid }) {
 
     if (valid) {
 
-      const items = this.afDB.list('/users/' + value['uid'] + '/inbox');    
-
+      const items = this.afDB.list('/users/' + value['uid'] + '/inbox');
       items.push({
         'message': 'You have been invited to use shared wallet with ' + this.status['name'],
         'wid': value.wallet,
-        'status': 'pending',        
+        'status': 'pending',
         'author': this.status['name']
       });
 
-      
-
-      this.afDB.object('/wallets/'+value.wallet+'/users/'+value['uid']).set(true);          
-
+      this.afDB.object('/wallets/' + value.wallet + '/users/' + value['uid']).set(true);
       this.shareWalletForm.controls['uid'].patchValue('');
 
     }
-
   }
 
   constructor(
     private shareWalletFormService: ShareWalletFormService,
     private afDB: AngularFireDatabase,
-    private status: StatusService,
-    private route: ActivatedRoute,
-    private router: Router
+    private status: StatusService
   ) { }
 
   ngOnInit() {
-
     this.shareWalletForm = this.shareWalletFormService.getShareWalletForm();
-    this.route.params.subscribe(params => {
-      this.shareWalletForm.controls['wallet'].patchValue(params['id']);
-    });
+    this.shareWalletForm.controls['wallet'].patchValue(this.wallet);
   }
 
 }
